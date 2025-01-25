dea202941c685438246f9e53fde57aef2cc4a15e
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-25 16:13:20 +0700

    berkomunikasi dengan API dan mengelola data penayangan di aplikasi

--- app/src/main/java/com/example/uas/repository/PenayanganRepository.kt
+++ app/src/main/java/com/example/uas/repository/PenayanganRepository.kt
@@ -18,3 +18,43 @@ interface PenayanganRepository {
     suspend fun getPenayanganById(id_penayangan: String): Penayangan
 }
 
+////berkomunikasi dengan API dan mengelola data penayangan di aplikasi
+class NetworkPenayanganRepository(
+    private val penayanganApiService: PenayanganService
+) : PenayanganRepository {
+    override suspend fun getPenayangan(): List<Penayangan> = penayanganApiService.getPenayangan()
+
+    /*override suspend fun getPenayangan(): PenayanganResponse {
+        return penayanganApiService.getPenayangan()
+    }*/
+
+    override suspend fun insertPenayangan(penayangan: Penayangan) {
+        penayanganApiService.insertPenayangan(penayangan)
+    }
+
+    override suspend fun updatePenayangan(id_penayangan: String, penayangan: Penayangan) {
+        penayanganApiService.updatePenayangan(id_penayangan, penayangan)
+    }
+
+    override suspend fun deletePenayangan(id_penayangan: String) {
+        try {
+            val response = penayanganApiService.deletePenayangan(id_penayangan)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Penayangan. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getPenayanganById(id_penayangan: String): Penayangan {
+        return penayanganApiService.getPenayanganById(id_penayangan)
+    }
+
+    /*override suspend fun getPenayanganById(id_penayangan: String): PenayanganDetailResponse {
+        return penayanganApiService.getPenayanganById(id_penayangan)
+    }*/
+}
