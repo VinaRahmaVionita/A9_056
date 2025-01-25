1326a3e45f8a76e40e5bb470dd82892f55e5b929
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-25 17:03:13 +0700

    mendefinisikan rute navigasi untuk halaman film

--- app/src/main/java/com/example/uas/ui/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/uas/ui/navigasi/DestinasiNavigasi.kt
@@ -11,3 +11,26 @@ object DestinasiHomeMenu : DestinasiNavigasi {
     override val titleRes = "Home Menu"
 }
 
+///FILM
+object DestinasiDetailFilm : DestinasiNavigasi {
+    override val route = "detailfilm"
+    override val titleRes = "Detail Film"
+    const val id_film = "id_film"
+    val routeWithArgs = "$route/{$id_film}"
+}
+
+object DestinasiHomeFilm : DestinasiNavigasi {
+    override val route = "homefilm"
+    override val titleRes = "Home Film"
+}
+
+object DestinasiInsertFilm : DestinasiNavigasi {
+    override val route = "insertfilm"
+    override val titleRes = "Insert Film"
+}
+
+object DestinasiUpdateFilm : DestinasiNavigasi {
+    override val route = "updatefilm"
+    override val titleRes = "Update Film"
+}
+
