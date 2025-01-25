1b674192bd96035215d01d73132075f3d8fc2b5a
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-25 16:16:55 +0700

    berkomunikasi dengan API dan mengelola data tiket di aplikasi

--- app/src/main/java/com/example/uas/repository/TiketRepository.kt
+++ app/src/main/java/com/example/uas/repository/TiketRepository.kt
@@ -15,3 +15,43 @@ interface TiketRepository {
     suspend fun deleteTiket(id_tiket: String)
 }
 
+//berkomunikasi dengan API dan mengelola data studio di aplikasi
+class NetworkTiketRepository(
+    private val tiketApiService: TiketService
+) : TiketRepository {
+    override suspend fun getTiket(): List<Tiket> = tiketApiService.getTiket()
+    override suspend fun getTiketById(id_tiket: String): Tiket {
+        return tiketApiService.getTiketById(id_tiket)
+    }
+
+    /*override suspend fun getTiket(): TiketResponse {
+        return tiketApiService.getTiket()
+    }*/
+
+    /*override suspend fun getTiketById(id_tiket: String): TiketDetailResponse {
+        return tiketApiService.getTiketById(id_tiket)
+    }*/
+
+
+    override suspend fun insertTiket(tiket: Tiket) {
+        tiketApiService.insertTiket(tiket)
+    }
+
+    override suspend fun updateTiket(id_tiket: String, tiket: Tiket) {
+        tiketApiService.updateTiket(id_tiket, tiket)
+    }
+
+    override suspend fun deleteTiket(id_tiket: String) {
+        try {
+            val response = tiketApiService.deleteTiket(id_tiket)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Tiket. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+}
