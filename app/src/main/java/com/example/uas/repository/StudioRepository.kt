762f487ad4e1aada5d71df2ca0833b14b1ba6517
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-25 16:15:11 +0700

    berkomunikasi dengan API dan mengelola data studio di aplikasi

--- app/src/main/java/com/example/uas/repository/StudioRepository.kt
+++ app/src/main/java/com/example/uas/repository/StudioRepository.kt
@@ -17,3 +17,43 @@ interface StudioRepository {
     suspend fun getStudioById(id_studio: String): Studio
 }
 
+//berkomunikasi dengan API dan mengelola data studio di aplikasi
+class NetworkStudioRepository(
+    private val studioApiService: StudioService
+) : StudioRepository {
+    override suspend fun getStudio(): List<Studio> = studioApiService.getStudio()
+
+    /*override suspend fun getStudio(): StudioResponse {
+        return studioApiService.getStudio()
+    }*/
+
+    override suspend fun insertStudio(studio: Studio) {
+        studioApiService.insertStudio(studio)
+    }
+
+    override suspend fun updateStudio(id_studio: String, studio: Studio) {
+        studioApiService.updateStudio(id_studio, studio)
+    }
+
+    override suspend fun deleteStudio(id_studio: String) {
+        try {
+            val response = studioApiService.deleteStudio(id_studio)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Studio. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getStudioById(id_studio: String): Studio {
+        return studioApiService.getStudioById(id_studio)
+    }
+
+    /*override suspend fun getStudioById(id_studio: String): StudioDetailResponse {
+        return studioApiService.getStudioById(id_studio)
+    }*/
+}
