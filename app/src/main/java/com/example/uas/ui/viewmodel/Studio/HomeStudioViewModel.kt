9e8f2bca5000379d87bf1596f33bfb320e5da769
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-27 20:48:21 +0700

    class utama yang mengelola beranda studio

--- app/src/main/java/com/example/uas/ui/viewmodel/Studio/HomeStudioViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/Studio/HomeStudioViewModel.kt
@@ -18,3 +18,40 @@ sealed class HomeStudioUiState {
     object Error : HomeStudioUiState()
     object Loading : HomeStudioUiState()
 }
+//Mengelola state UI untuk beranda studio.
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+class HomeStudioViewModel (private val std: StudioRepository) : ViewModel(){
+    //State utama yang merepresentasikan status UI
+    var stdUiState: HomeStudioUiState by mutableStateOf(HomeStudioUiState.Loading)
+        private set
+
+    init {
+        getStudio()
+    }
+    //Mengambil daftar studio dari database
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun getStudio() {
+        viewModelScope.launch {
+            stdUiState = HomeStudioUiState.Loading
+            stdUiState = try {
+                HomeStudioUiState.Success(std.getStudio())
+            } catch (e: IOException){
+                HomeStudioUiState.Error
+            } catch (e: HttpException) {
+                HomeStudioUiState.Error
+            }
+        }
+    }
+    //Menghapus data studio berdasarkan id_studio
+    fun deleteStudio(id_studio: String){
+        viewModelScope.launch {
+            try {
+                std.deleteStudio(id_studio)
+            }catch (e: IOException){
+                HomeStudioUiState.Error
+            }catch (e: HttpException){
+                HomeStudioUiState.Error
+            }
+        }
+    }
+}
