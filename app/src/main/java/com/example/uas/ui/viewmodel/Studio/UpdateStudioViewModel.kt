df552b0bf7df5c35605b24e16d2b8204ed0a652e
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-27 20:53:11 +0700

    menyimpan status ui

--- app/src/main/java/com/example/uas/ui/viewmodel/Studio/UpdateStudioViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/Studio/UpdateStudioViewModel.kt
@@ -11,6 +11,11 @@ import com.example.uas.repository.StudioRepository
 import kotlinx.coroutines.launch
 
 
+//menyimpan status ui
+data class UpdateStudioUiState(
+    val studioEvent: UpdateStudioUiEvent = UpdateStudioUiEvent(),
+    val snackBarMessage: String? = null
+)
 //Data yang mewakili input pengguna, seperti id_studio, nama_studio, dan kapasitas
 data class UpdateStudioUiEvent(
     val id_studio: String = "",
