583bfc5aa4bc405bd6d3aa267f952b62118918ad
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-27 20:51:29 +0700

    Mengelola logika aplikasi untuk operasi insert dan update data studio

--- app/src/main/java/com/example/uas/ui/viewmodel/Studio/InsertStudioViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/Studio/InsertStudioViewModel.kt
@@ -10,7 +10,38 @@ import com.example.uas.repository.StudioRepository
 import kotlinx.coroutines.launch
 
 //Mengelola logika aplikasi untuk operasi penambahan dan pembaruan data studio
-
+class InsertStudioViewModel(private val std: StudioRepository): ViewModel() {
+    var uiState by mutableStateOf(InsertStudioUiState())
+        private set
+    //Memperbarui state dengan data input pengguna
+    fun updateInsertStdState(insertStudioUiEvent: InsertStudioUiEvent){
+        uiState = InsertStudioUiState(insertStudioUiEvent = insertStudioUiEvent)
+    }
+    //Menyimpan data baru ke database
+    suspend fun InsertStd(){
+        viewModelScope.launch {
+            try {
+                std.insertStudio(uiState.insertStudioUiEvent.toStd())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+    //Memperbarui data studio yang sudah ada di database berdasarkan id_studio
+    suspend fun updateStd(id_studio: String){
+        viewModelScope.launch {
+            try {
+                std.updateStudio(id_studio, uiState.insertStudioUiEvent.toStd())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+//Mengubah objek Studio (data dari database) menjadi state UI
+fun Studio.toUiStateStd(): InsertStudioUiState = InsertStudioUiState(
+    insertStudioUiEvent = toInsertStudioUiEvent()
+)
 //Mengubah state UI (InsertStudioUiEvent) menjadi objek Studio untuk disimpan ke database
 fun InsertStudioUiEvent.toStd(): Studio = Studio(
     id_studio = id_studio,
