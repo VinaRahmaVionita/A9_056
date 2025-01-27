8e5dd0dd3f8a2d2fba0edbb89d236ee7d6c18b4b
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-27 21:48:09 +0700

    kelas ViewModel yang berfungsi untuk mengelola data dan status UI terkait detail film

--- app/src/main/java/com/example/uas/ui/viewmodel/Film/DetailFilmViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/Film/DetailFilmViewModel.kt
@@ -12,7 +12,56 @@ import com.example.uas.ui.navigasi.DestinasiDetailFilm
 import kotlinx.coroutines.launch
 
 
+class DetailFilmViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val filmRepository: FilmRepository
+) : ViewModel() {
+    private val id_film: String = checkNotNull(savedStateHandle[DestinasiDetailFilm.id_film])
 
+    var detailFilmUiState: DetailFilmUiState by mutableStateOf(DetailFilmUiState())
+        private set
+
+    init {
+        getFilmById()
+    }
+
+    private fun getFilmById(){
+        viewModelScope.launch {
+            detailFilmUiState = DetailFilmUiState(isLoading = true)
+            try {
+                val result = filmRepository.getFilmById(id_film)
+                detailFilmUiState = DetailFilmUiState(
+                    detailFilmUiEvent = result.toDetailFilmUiEvent(),
+                    isLoading = false
+                )
+            } catch (e: Exception) {
+                detailFilmUiState = DetailFilmUiState (
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Unknown"
+                )
+            }
+        }
+    }
+
+    fun deleteFilm(){
+        viewModelScope.launch {
+            detailFilmUiState = DetailFilmUiState(isLoading = true)
+            try {
+                filmRepository.deleteFilm(id_film)
+
+                detailFilmUiState = DetailFilmUiState(isLoading = false)
+            } catch (e: Exception){
+                detailFilmUiState = DetailFilmUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Unknown Error"
+                )
+            }
+        }
+    }
+
+}
 
 data class DetailFilmUiState(
     val detailFilmUiEvent: InsertFilmUiEvent = InsertFilmUiEvent(),
