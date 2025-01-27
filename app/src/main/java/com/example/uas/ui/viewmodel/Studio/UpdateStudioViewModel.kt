00720f788f274f309a68652627837b4b42468687
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-27 20:52:45 +0700

    data yang mewakili input pengguna

---
+++ app/src/main/java/com/example/uas/ui/viewmodel/Studio/UpdateStudioViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.uas.ui.viewmodel.Studio
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uas.model.Studio
+import com.example.uas.repository.StudioRepository
+import kotlinx.coroutines.launch
+
+
+//Data yang mewakili input pengguna, seperti id_studio, nama_studio, dan kapasitas
+data class UpdateStudioUiEvent(
+    val id_studio: String = "",
+    val nama_studio: String = "",
+    val kapasitas: String = ""
+)
