c686514ba3ac193bd1d82cd77896360b68214f17
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-27 21:25:07 +0700

    Mengonversi objek Penayangan menjadi InsertTayangUiEvent

--- app/src/main/java/com/example/uas/ui/viewmodel/penayangan/InsertPenayanganViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/penayangan/InsertPenayanganViewModel.kt
@@ -10,6 +10,14 @@ import com.example.uas.repository.PenayanganRepository
 import kotlinx.coroutines.launch
 
 
+//Mengonversi objek Penayangan menjadi InsertTayangUiEvent
+fun Penayangan.toInsertTayangUiEvent(): InsertTayangUiEvent = InsertTayangUiEvent(
+    id_penayangan = id_penayangan,
+    id_film = id_film,
+    id_studio = id_studio,
+    tanggal_penayangan = tanggal_penayangan,
+    harga_tiket = harga_tiket
+)
 //Data penayangan yang akan ditambahkan atau diperbarui
 data class InsertTayangUiState(
     val insertTayangUiEvent: InsertTayangUiEvent = InsertTayangUiEvent()
