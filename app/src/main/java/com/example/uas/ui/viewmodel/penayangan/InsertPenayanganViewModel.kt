234d6fcc1377ae8c7bd5ffd5017e9b7c2ec00b63
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-27 21:26:02 +0700

    class utama untuk mengelola logikan insert penayangan

--- app/src/main/java/com/example/uas/ui/viewmodel/penayangan/InsertPenayanganViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/penayangan/InsertPenayanganViewModel.kt
@@ -9,7 +9,40 @@ import com.example.uas.model.Penayangan
 import com.example.uas.repository.PenayanganRepository
 import kotlinx.coroutines.launch
 
+//ogika untuk menambah data penayangan ke dalam repository
+class InsertPenayanganViewModel (private val tayang: PenayanganRepository): ViewModel() {
+    //Menyimpan data sementara untuk operasi penambahan atau pembaruan
+    var uiState by mutableStateOf(InsertTayangUiState())
+        private set
+    //Memperbarui data penayangan yang sudah ada
+    fun updateInsertTayangState(insertTayangUiEvent: InsertTayangUiEvent){
+        uiState = InsertTayangUiState(insertTayangUiEvent = insertTayangUiEvent)
+    }
 
+    suspend fun insertTayang(){
+        viewModelScope.launch {
+            try {
+                tayang.insertPenayangan(uiState.insertTayangUiEvent.toTayang())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+    // untuk memperbarui data penayangan berdasarkan ID
+    suspend fun updateTayang(id_penayangan: String){
+        viewModelScope.launch {
+            try {
+                tayang.updatePenayangan(id_penayangan, uiState.insertTayangUiEvent.toTayang())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+fun Penayangan.toUiStateTayang(): InsertTayangUiState = InsertTayangUiState(
+    insertTayangUiEvent = toInsertTayangUiEvent()
+)
 //Mengonversi objek InsertTayangUiEvent menjadi Penayangan
 fun InsertTayangUiEvent.toTayang(): Penayangan = Penayangan(
     id_penayangan = id_penayangan,
