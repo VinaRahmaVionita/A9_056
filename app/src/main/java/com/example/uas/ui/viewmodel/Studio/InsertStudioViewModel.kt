1881e23f3fad574b23cc68216a0326b2d9867cee
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-27 20:50:41 +0700

    mengubah state ui menjadi objek studio untuk disimpan ke database

--- app/src/main/java/com/example/uas/ui/viewmodel/Studio/InsertStudioViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/Studio/InsertStudioViewModel.kt
@@ -9,7 +9,14 @@ import com.example.uas.model.Studio
 import com.example.uas.repository.StudioRepository
 import kotlinx.coroutines.launch
 
+//Mengelola logika aplikasi untuk operasi penambahan dan pembaruan data studio
 
+//Mengubah state UI (InsertStudioUiEvent) menjadi objek Studio untuk disimpan ke database
+fun InsertStudioUiEvent.toStd(): Studio = Studio(
+    id_studio = id_studio,
+    nama_studio = nama_studio,
+    kapasitas = kapasitas
+)
 //Mengubah objek Studio menjadi event UI (InsertStudioUiEvent)
 fun Studio.toInsertStudioUiEvent(): InsertStudioUiEvent = InsertStudioUiEvent(
     id_studio = id_studio,
