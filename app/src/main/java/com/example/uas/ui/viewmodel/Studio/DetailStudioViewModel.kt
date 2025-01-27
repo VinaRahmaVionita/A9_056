5f3bca12ae5786749ae88f17e3ffa86d395424cf
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-27 20:47:06 +0700

    class utama yang mengelola detail studio

--- app/src/main/java/com/example/uas/ui/viewmodel/Studio/DetailStudioViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/Studio/DetailStudioViewModel.kt
@@ -11,7 +11,54 @@ import com.example.uas.repository.StudioRepository
 import com.example.uas.ui.navigasi.DestinasiDetailStudio
 import kotlinx.coroutines.launch
 
+class DetailStudioViewModel(
+    savedStateHandle: SavedStateHandle,//Menyimpan data id_studio yang diteruskan dari layar sebelumnya
+    private val studioRepository: StudioRepository //Repository untuk operasi database.
+) : ViewModel() {
+    private val id_studio: String = checkNotNull(savedStateHandle[DestinasiDetailStudio.id_studio])
 
+    var detailStudioUiState: DetailStudioUiState by mutableStateOf(DetailStudioUiState())
+        private set
+
+    init {
+        getStudioById()
+    }
+    //Mengambil data detail studio berdasarkan id_studio
+    private fun getStudioById() {
+        viewModelScope.launch {
+            detailStudioUiState = DetailStudioUiState(isLoading = true)
+            try {
+                val result = studioRepository.getStudioById(id_studio)
+                detailStudioUiState = DetailStudioUiState(
+                    detailStudioUiEvent = result.toDetailStudioUiEvent(),
+                    isLoading = false
+                )
+            } catch (e: Exception) {
+                detailStudioUiState = DetailStudioUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Unknown"
+                )
+            }
+        }
+    }
+    //Menghapus studio dari database berdasarkan id_studio
+    fun deleteStd() {
+        viewModelScope.launch {
+            detailStudioUiState = DetailStudioUiState(isLoading = true)
+            try {
+                studioRepository.deleteStudio(id_studio)
+                detailStudioUiState = DetailStudioUiState(isLoading = false)
+            } catch (e: Exception) {
+                detailStudioUiState = DetailStudioUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Unknown Error"
+                )
+            }
+        }
+    }
+}
 //memuat informasi tentang kondisi data
 data class DetailStudioUiState(
     val detailStudioUiEvent: InsertStudioUiEvent = InsertStudioUiEvent(),
