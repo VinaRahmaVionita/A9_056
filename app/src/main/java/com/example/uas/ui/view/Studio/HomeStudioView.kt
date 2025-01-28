1a693503faf4015edcb918aa58cdbbc58f76da10
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-28 12:18:08 +0700

    menampilkan data studio di dalam card

---
+++ app/src/main/java/com/example/uas/ui/view/Studio/HomeStudioView.kt
@@ -0,0 +1,99 @@
+package com.example.uas.ui.view.Studio
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uas.R
+import com.example.uas.model.Studio
+import com.example.uas.ui.CustomWidget.CostumeTopAppBar
+import com.example.uas.ui.navigasi.DestinasiHomeStudio
+import com.example.uas.ui.view.Film.OnLoading
+import com.example.uas.ui.viewmodel.PenyediaViewModel
+import com.example.uas.ui.viewmodel.Studio.HomeStudioUiState
+import com.example.uas.ui.viewmodel.Studio.HomeStudioViewModel
+
+
+
+@Composable
+fun StudioCard(
+    studio: Studio,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Studio) -> Unit = {}
+){
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFBBDEFB) // Warna biru muda
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = studio.id_studio,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(studio) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                    )
+                }
+                Text(
+                    text = studio.nama_studio,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(
+                text = studio.kapasitas,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
