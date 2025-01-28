f93beb343b06c1dbad3c89771b38b69ee829a105
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-28 12:15:42 +0700

    menmapilkan label beserta isinya

--- app/src/main/java/com/example/uas/ui/view/Studio/DetailStudioView.kt
+++ app/src/main/java/com/example/uas/ui/view/Studio/DetailStudioView.kt
@@ -47,6 +47,30 @@ import com.example.uas.ui.viewmodel.Studio.toStd
 
 
 @Composable
+fun ComponentDetailStd(
+    modifier: Modifier = Modifier,
+    judul:String,
+    isinya:String
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
 private fun DeleteConfirmationDialog(
     onDeleteConfirm: () -> Unit,
     onDeleteCancel: () -> Unit,
